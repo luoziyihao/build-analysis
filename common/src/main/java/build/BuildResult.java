a45ff3edf75298463c74f85eba8a394539d709e6
earliest <earliest@hotmail.com>
2017-04-10 23:08:06 -0700

    add BuildResult class

---
+++ common/src/main/java/build/BuildResult.java
@@ -0,0 +1,39 @@
+package build;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class BuildResult {
+	
+	Map<String, List<Result>> buildResultMapping;
+	
+	public BuildResult(){
+		buildResultMapping = new HashMap<>();
+	}
+	
+	public void addResultEntry(String id, String path, boolean success, String reason, Date buildTime){
+		Result newResult = new Result();
+		newResult.success = success;
+		newResult.specificReason = SpecificReason.valueOf(reason);
+		newResult.path = path;
+		newResult.buildTime = buildTime;
+		if(buildResultMapping.get(id) == null){
+			List<Result> results = new LinkedList<>();
+			buildResultMapping.put(id, results);
+		}
+		buildResultMapping.get(id).add(newResult);
+	}
+	
+	public List<Result> getResult(String id){
+		return buildResultMapping.get(id);
+	}
+	
+	public Iterator<String> getIds(){
+		return buildResultMapping.keySet().iterator();
+	}
+	
+}
---
+++ common/src/main/java/build/Result.java
@@ -0,0 +1,10 @@
+package build;
+
+import java.util.Date;
+
+public class Result {
+	public Date buildTime;
+	public String path;
+	public boolean success = false;
+	public SpecificReason specificReason;
+}

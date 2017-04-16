a3d6cf4d211c70187c876517478f0840e59bada5
earliest <earliest@hotmail.com>
2017-04-16 15:36:22 -0700

    add accessors the data classes

--- common/src/main/java/com/coding/common/build/BuildResult.java
+++ common/src/main/java/com/coding/common/build/BuildResult.java
@@ -10,12 +10,13 @@ public class BuildResult {
 		buildResultMapping = new HashMap<>();
 	}
 	
-	public void addResultEntry(String id, String path, boolean success, String reason, Date buildTime){
+	public void addResultEntry(String id, String path, boolean success, String reason, Date buildTime, PomInfo pomInfo){
 		Result newResult = new Result();
-		newResult.success = success;
-		newResult.specificReason = SpecificReason.valueOf(reason);
-		newResult.path = path;
-		newResult.buildTime = buildTime;
+		newResult.success(success);
+		newResult.specificReason(SpecificReason.valueOf(reason));
+		newResult.path(path);
+		newResult.buildTime(buildTime);
+		newResult.pomInfo(pomInfo);
 		if(buildResultMapping.get(id) == null){
 			List<Result> results = new LinkedList<>();
 			buildResultMapping.put(id, results);

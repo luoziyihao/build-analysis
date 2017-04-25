711351a8fe21cc4d30e6efbfc96c6de432dfc3f9
luoziyihao <wangyiraoxiang@163.com>
2017-04-25 23:33:41 +0800

    resolve conflics, modify pplication.properties

--- build/src/main/java/com/coding/build/builder/BuilderImpl.java
+++ build/src/main/java/com/coding/build/builder/BuilderImpl.java
@@ -1,101 +1,87 @@
 package com.coding.build.builder;
 
-import java.io.File;
+import com.coding.build.executor.Executor;
+import com.coding.build.parser.Parser;
+import com.coding.build.validator.Validator;
+import org.apache.log4j.Logger;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import org.apache.log4j.Logger;
-import org.apache.maven.shared.invoker.MavenInvocationException;
+public class BuilderImpl implements Builder {
 
-import com.coding.build.executor.Executor;
-import com.coding.build.parser.Parser;
-import com.coding.build.parser.ParserFailException;
-import com.coding.build.validator.Validator;
-
-public class BuilderImpl implements Builder{
-
-	protected Executor executor = null;
-	
-	
-	public Parser jsonParser = null;
-	public Validator validator = null;
-	
-	private final Logger logger = Logger.getLogger(this.getClass());
-	public BuilderImpl(){
-<<<<<<< HEAD
-		
-=======
-		maven_home_path = System.getenv(MAVEN_HOME_TAG);
-		System.out.println("maven_home_path=" + maven_home_path);
->>>>>>> 967e35d268405c80cf8be6519205a43dddb7de56
-	}
-	
-
-	
-	@Override
-	public boolean build(Group target) {
-		logger.debug("Going to build target: " + target);
-		if(executor != null){
-			try {
-				executor.setMavenHome(BuilderConfiguration.maven_home_path);
-				executor.process(target);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return false;
-	}
-	
-	public void setExecutor(Executor executor){
-		this.executor = executor;
-	}
-
-	public void setParser(Parser parser){
-		this.jsonParser = parser;
-	}
-	
-	public void setValidator(Validator validator){
-		this.validator = validator;
-	}
+    protected Executor executor = null;
 
 
-	@Override
-	public List<Group> fetchGroups(String projectRoot) {
-		List<Group> groups = new LinkedList<>();
-		Path p = Paths.get(projectRoot);
-		
-		Predicate<Path> pfilter = path-> {
-			return path.getFileName().toString().matches(".*group[0-9]+");
-		};
-		try {
-			groups = Files.list(p).filter(pfilter).map(path -> {
-				return jsonParser.parse(path.toString());
-			}).filter(g -> g!= null).collect(Collectors.toList());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return groups;
-	}
+    public Parser jsonParser = null;
+    public Validator validator = null;
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    public BuilderImpl() {
+
+    }
 
 
+    @Override
+    public boolean build(Group target) {
+        logger.debug("Going to build target: " + target);
+        if (executor != null) {
+            try {
+                executor.setMavenHome(BuilderConfiguration.maven_home_path);
+                executor.process(target);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
 
-	@Override
-	public boolean buildAll(List<Group> groups) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
+        return false;
+    }
+
+    public void setExecutor(Executor executor) {
+        this.executor = executor;
+    }
+
+    public void setParser(Parser parser) {
+        this.jsonParser = parser;
+    }
+
+    public void setValidator(Validator validator) {
+        this.validator = validator;
+    }
+
+
+    @Override
+    public List<Group> fetchGroups(String projectRoot) {
+        List<Group> groups = new LinkedList<>();
+        Path p = Paths.get(projectRoot);
+
+        Predicate<Path> pfilter = path -> {
+            return path.getFileName().toString().matches(".*group[0-9]+");
+        };
+        try {
+            groups = Files.list(p).filter(pfilter).map(path -> {
+                return jsonParser.parse(path.toString());
+            }).filter(g -> g != null).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return groups;
+    }
+
+
+    @Override
+    public boolean buildAll(List<Group> groups) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
 
 }

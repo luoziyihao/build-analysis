43a6ef75b0ba6368f9167b2b799919eebf4caa27
earliest <earliest@hotmail.com>
2017-05-11 22:25:27 -0700

    modified specific reason

--- common/src/main/java/com/coding/common/build/SpecificReason.java
+++ common/src/main/java/com/coding/common/build/SpecificReason.java
@@ -1,16 +1,9 @@
 package com.coding.common.build;
 
 public enum SpecificReason {
-	SUCCESS, 
-	INVALID_FORMAT, 
-	MAVEN_POM_NOT_FOUND,
+	SUCCESS,  //everything went well. surefire-report generated
+	MAVEN_VALIDATION_ERROR, // mvn validate fail, including missing pom, faulty pom format
+	MAVEN_COMPILE_FAILED, //mvn compile fail
 	NO_SUCH_DIRECTORY, //The path specified in JSON configuration for a member is not found
-	INVALID_JSON_CONFIG, //JSON file has a faulty format
-
-      FAILED
-    , CONFIG_ILLEGAL
-    , COMPILE_FAILED
-    , TEST_FAILED
-    , TEST_SKIPPED
-    , DEPENDENCY_FAILED
+	INVALID_JSON_CONFIG //JSON file has a faulty format,
 }
